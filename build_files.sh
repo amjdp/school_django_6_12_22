fcb39bbf3847c9371ec32cf0eccaa7d311f43d3b
amjdp <athiramj123@gmail.com>
2022-12-06 16:40:01 +0530

    vercel deploy

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo " BUILD START"
-python3.10.4 pip install -r requirements.txt
-python3.10.4 manage.py collectstatic
+python3.9 pip install -r requirements.txt
+python3.9 manage.py collectstatic
 
 echo " BUILD END"
